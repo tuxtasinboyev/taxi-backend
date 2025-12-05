1b78b8a0ae3399da57e9fe0e0e992a4c009ada0f
Tuxtasinboyev Omadbek <tuxtasinboyev@kali.kali>
2025-12-05 20:28:41 +0500

    push buldi

--- src/main.ts
+++ src/main.ts
@@ -37,7 +37,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api/docs', app, document);
 
-  await app.listen(process.env.PORT ?? 3000);
+  await app.listen(process.env.PORT ?? 3000, '0.0.0.0');
   console.log(`🚀 Server is running on http://localhost:${process.env.PORT ?? 3000}`);
   console.log(`📖 Swagger docs: http://localhost:${process.env.PORT ?? 3000}/api/docs`);
 }
